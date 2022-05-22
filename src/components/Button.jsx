df84c8717a31f9be5c8dcaaa4e66fa0523bbc515
Artyom Konstantinov <konstantinov050701@gmail.com>
2022-05-22 14:39:06 +0300

    Minor fixes.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Button = ({ text="Хочу следить за проектом" }) => {
-  return <button>{text}</button>
+const Button = ({ text = "Хочу следить за проектом" }) => {
+  return <button className="button-primary">{text}</button>
 }
 
 export default Button
