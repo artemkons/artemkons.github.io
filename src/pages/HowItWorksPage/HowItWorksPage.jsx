934c70b639af806de0d1899fd8ead95f21f3ee5d
Artyom Konstantinov <konstantinov050701@gmail.com>
2022-05-23 16:17:06 +0300

    Fixed how it works page layout

--- src/pages/HowItWorksPage/HowItWorksPage.jsx
+++ src/pages/HowItWorksPage/HowItWorksPage.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Button from '../../components/Button'
+import React from 'react';
+import Button from '../../components/Button';
 
-import "./how-it-works.sass"
+import './how-it-works.sass';
 
 const HowItWorksPage = () => {
-  return <>
-    <div className="how-it-works-page">
-      <h1>Как работают сценарии?</h1>
-      <img height={200} src="https://bipbap.ru/wp-content/uploads/2017/04/2-8.jpg" alt="Красивая картинка" />
-      <p>Всё очень просто:
-        Скачиваете приложение
-        Находите в маркетплейсе по ключевым словам нужный сценарий
-        Запускаете и разрешаете ему необходимые доступы
-        Вуа-ля, ваше устройство работает без вас
-      </p>
-    </div>
-    <Button>Хочу следить за проектом</Button>
-  </>
-}
+    return (
+        <div className='how-it-works-page'>
+            <h1>Как работают сценарии?</h1>
+            <img
+                src='https://bipbap.ru/wp-content/uploads/2017/04/2-8.jpg'
+                alt='Красивая картинка'
+            />
+            <p>
+                Всё очень просто: Скачиваете приложение Находите в маркетплейсе
+                по ключевым словам нужный сценарий Запускаете и разрешаете ему
+                необходимые доступы Вуа-ля, ваше устройство работает без вас
+            </p>
+            <Button>Хочу следить за проектом</Button>
+        </div>
+    );
+};
 
-export default HowItWorksPage
+export default HowItWorksPage;
