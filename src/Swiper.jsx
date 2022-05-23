df9b18057ea5a50d9e2d7afb3228c416bf722b1e
Artyom Konstantinov <konstantinov050701@gmail.com>
2022-05-23 17:16:07 +0300

    FIx: problems with slider on mobile

--- src/Swiper.jsx
+++ src/Swiper.jsx
@@ -8,37 +8,43 @@ import HowItWorksPage from './pages/HowItWorksPage/HowItWorksPage';
 import OurBenefitsPage from './pages/OurBenefitsPage/OurBenefitsPage';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Scrollbar, Mousewheel } from 'swiper';
+import { Pagination, Scrollbar, Mousewheel, FreeMode } from 'swiper';
+import { useMediaQuery } from '@chakra-ui/react';
 
 const pages = [
-  MainPage,
-  AboutProjectPage,
-  //AboutScenarioPage,
-  HowItWorksPage,
-  OurBenefitsPage
+    <MainPage />,
+    <AboutProjectPage />,
+    //AboutScenarioPage,
+    <HowItWorksPage />,
+    <OurBenefitsPage />
 ];
 
 const SwiperComponent = () => {
-  return (
-    <Swiper
-      direction={'vertical'}
-      pagination={{
-        clickable: true,
-        renderBullet: function(index, className) {
-          return `<span class="${className}"><span class="swiper-pagination-bullet__slider-counter">${index + 1
-            }</span></span>`;
-        }
-      }}
-      mousewheel={true}
-      scrollbar={{ draggable: true }}
-      modules={[Pagination, Scrollbar, Mousewheel]}
-      className='main-swiper'
-    >
-      {pages.map((page, i) => (
-        <SwiperSlide key={i}>{page}</SwiperSlide>
-      ))}
-    </Swiper>
-  );
+    const [isMobile] = useMediaQuery('(max-width: 450px)');
+
+    return isMobile ? (
+        <>{[pages]}</>
+    ) : (
+        <Swiper
+            direction={'vertical'}
+            pagination={{
+                clickable: true,
+                renderBullet: function (index, className) {
+                    return `<span class="${className}"><span class="swiper-pagination-bullet__slider-counter">${
+                        index + 1
+                    }</span></span>`;
+                }
+            }}
+            mousewheel={true}
+            scrollbar={{ draggable: true }}
+            modules={[Pagination, Scrollbar, Mousewheel]}
+            className='main-swiper'
+        >
+            {pages.map((page, i) => (
+                <SwiperSlide key={i}>{page}</SwiperSlide>
+            ))}
+        </Swiper>
+    );
 };
 
 export default SwiperComponent;
